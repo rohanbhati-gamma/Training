282e840ffe331fd4767e61c97daad02bf351940b
rohanbhati-gamma <rohan.bhati@gamma.io>
2025-05-30 11:00:41 +0530

    firstCommit

---
+++ Day1/modules.py
@@ -0,0 +1,14 @@
+def Greet():
+    print('Good Morning')
+
+location='Indore'
+
+class Employee():
+    def __init__(self, name, location):
+        self.name = name
+        self.location = location
+
+    def show(self):
+        print(self.name)
+        print(self.location)
+
